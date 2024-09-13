9b4cc4ad20fc0c3ae40c3b030331b3949c7f1650
Vladimir Teslenko <vovateslenko.1996.28@gmail.com>
2024-09-14 00:29:45 +0200

    Тесты

---
+++ tests/test_oop.py
@@ -0,0 +1,15 @@
+import pytest
+from src.classes import Category, Product
+
+def test_category(category):
+    assert category.name == 'Смартфоны'
+    assert category.description == 'Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни'
+    assert category.products_quantity == 3
+    assert category.categories_quantity == 1
+
+
+def test_products(products):
+    assert products.name == 'Xiaomi Redmi Note 11'
+    assert products.description == '1024GB, Синий'
+    assert products.quantity == 14
+
