03933750bc99696d9360f1ed3c9126bcd3724774
Vladimir Teslenko <vovateslenko.1996.28@gmail.com>
2024-09-14 00:28:19 +0200

    Добавлены фикстуры для тестов

---
+++ tests/conftest.py
@@ -0,0 +1,40 @@
+import pytest
+from src.classes import Category, Product
+
+
+@pytest.fixture
+def category():
+    return Category(
+        name="Смартфоны",
+        description="Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни",
+        products=[
+      {
+        "name": "Samsung Galaxy C23 Ultra",
+        "description": "256GB, Серый цвет, 200MP камера",
+        "price": 180000.0,
+        "quantity": 5
+      },
+      {
+        "name": "Iphone 15",
+        "description": "512GB, Gray space",
+        "price": 210000.0,
+        "quantity": 8
+      },
+      {
+        "name": "Xiaomi Redmi Note 11",
+        "description": "1024GB, Синий",
+        "price": 31000.0,
+        "quantity": 14
+      }
+    ]
+    )
+
+@pytest.fixture
+def products():
+    return Product(
+        name="Xiaomi Redmi Note 11",
+        description="1024GB, Синий",
+        price=31000.0,
+        quantity=14
+    )
+
