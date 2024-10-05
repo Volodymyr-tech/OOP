91953f85540c85ea5dccc2b8edc24717b3cc48a2
Vladimir Teslenko <vovateslenko.1996.28@gmail.com>
2024-10-05 17:56:21 +0200

    поправка

--- src/mixin.py
+++ src/mixin.py
@@ -1,7 +1,5 @@
-from abc import ABC, abstractmethod
 
-
-class PrintMixin(ABC):
+class PrintMixin():
 
     def __repr__(self):
         print(f'{self.__class__.__name__}, {self.description}, {self.price}, {self.quantity}')
